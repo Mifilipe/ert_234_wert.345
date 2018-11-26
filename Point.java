179e924fb421716e0c54c7a8d0b6b0559f9ae281
Mifilipe <45368963+Mifilipe@users.noreply.github.com>
2018-11-26 18:43:12 +0000

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,17 @@
+class Point {
+
+	//Atributos
+	
+	int x;
+	int y;
+	
+	//Construtor definido com atributos. Não esquecer chaveta.
+	
+	Point (int x, int y) {
+	
+	this.x = x;
+	this.y = y;
+	
+	}
+	
+}
